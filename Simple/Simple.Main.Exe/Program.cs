ba04d926ee8223f21e24c81ea5b5123d9e3fca38
Andre Kaufmann <Andre.Kaufmann@geutebrueck.com>
2020-11-01 20:52:48 +0100

    Added configuration example to switch between console and file output

--- Simple/Simple.Main.Exe/Program.cs
+++ Simple/Simple.Main.Exe/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Simple.Domain;
 using Simple.Domain.Main;
@@ -17,10 +18,22 @@ namespace Simple
         {
             Console.WriteLine("Hello World!");
             Host.CreateDefaultBuilder(args).
-                ConfigureServices(services =>
+                ConfigureAppConfiguration((hostContext, config)=>
                 {
-                    //services.AddSingleton<IOutput, OutputToConsole>(); // This is the port we want to use in our root object
-                    services.AddSingleton<IOutput, OutputToFileSimulated>(); // This is the port we want to use in our root object
+                    //config.SetBasePath(Environment.CurrentDirectory); // Default
+                    //config.AddJsonFile("appsettings.json", optional: false);
+                    //config.AddEnvironmentVariables();
+                }).
+                ConfigureServices((context, services) =>
+                {
+                    var settings = new ServicesSettings();
+                    context.Configuration.GetSection("ServicesSettings").Bind(settings);
+                                      
+                   
+                    if (settings.OutputDestination == OutputDestinations.File)
+                        services.AddSingleton<IOutput, OutputToFileSimulated>(); // This is the port we want to use in our root object
+                    else 
+                        services.AddSingleton<IOutput, OutputToConsole>(); // This is the port we want to use in our root object
                     services.AddSingleton<ISimpleDomainMainEntry, SimpleDomainMain>(); // This is the main entry of the domain
                     services.AddHostedService<SharedCallRoot>(); // This is a generic wrapper for main root
                 }).
